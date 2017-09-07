0b3093f9741b07ba6ac6f204de30d8375de40459
Administrador <Administrador@172.16.12.114>
2017-09-07 14:45:34 -0600

    Prueba de ejemplo

---
+++ src/proyecto/Proyecto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Proyecto {
+    
+    public static void main(String[] args) {
+        /* Prueba de proyecto en gifhub */
+        System.out.println("hola mundo Otra vez");
+    }
+    
+}
